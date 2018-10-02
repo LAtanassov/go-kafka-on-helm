3340f4ae2434736c43a7143c4a9d8aefcd310e7b
latanassov <latschesar.atanassov@gmx.at>
2018-10-02 21:12:09 +0200

    add feature - kafka producer

--- go-producer/cmd/go-producer/main.go
+++ go-producer/cmd/go-producer/main.go
@@ -8,7 +8,11 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
+	"strings"
+	"sync"
 	"time"
+
+	"github.com/Shopify/sarama"
 )
 
 func main() {
@@ -16,12 +20,20 @@ func main() {
 	var (
 		envHTTPAddr = envString("HTTP_ADDR", ":8080")
 		httpAddr    = *flag.String("http.addr", envHTTPAddr, "HTTP listen address")
+
+		envKafkaAddrs = envString("KAFKA_ADDRS", ":9092")
+		envKafkaTopic = envString("KAFKA_TOPIC", "timer")
+
+		kafkaAddrs = *flag.String("kafka.addrs", envKafkaAddrs, "comma-seperated kafka broker addresses")
+		kafkaTopic = *flag.String("kafka.topic", envKafkaTopic, "kafka topic")
 	)
 	flag.Parse()
 
+	// os signal trap
 	signals := make(chan os.Signal, 1)
 	signal.Notify(signals, os.Interrupt)
 
+	// health status endpoint
 	mux := http.NewServeMux()
 	mux.Handle("/_status/liveness", livenessHandler())
 	mux.Handle("/_status/readiness", readinessHandler())
@@ -37,9 +49,46 @@ func main() {
 		}
 	}(server)
 
-	<-signals
+	// kafka producer
+	config := sarama.NewConfig()
+	config.Producer.Return.Successes = true
+
+	producer, err := sarama.NewAsyncProducer(strings.Split(kafkaAddrs, ","), config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for range producer.Successes() {
+			log.Println("message acknowledged.")
+		}
+	}()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for err := range producer.Errors() {
+			log.Println(err)
+		}
+	}()
+
+ProducerLoop:
+	for {
+		select {
+		case <-time.After(5 * time.Second):
+			producer.Input() <- &sarama.ProducerMessage{Topic: kafkaTopic, Value: sarama.StringEncoder(time.Now().String())}
+			log.Println("message sent.")
+		case <-signals:
+			producer.AsyncClose()
+			break ProducerLoop
+		}
+	}
 
 	fmt.Println("gracefully shutdown...")
+	wg.Wait()
 
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
@@ -49,7 +98,6 @@ func main() {
 	}
 
 	log.Println("service stopped.")
-
 }
 
 func livenessHandler() http.Handler {
--- go-producer/go.mod
+++ go-producer/go.mod
@@ -1,1 +1,13 @@
 module github.com/LAtanassov/go-kafka-on-helm/go-producer
+
+require (
+	github.com/Shopify/sarama v1.19.0
+	github.com/Shopify/toxiproxy v2.1.3+incompatible // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/eapache/go-resiliency v1.1.0 // indirect
+	github.com/eapache/go-xerial-snappy v0.0.0-20180814174437-776d5712da21 // indirect
+	github.com/eapache/queue v1.1.0 // indirect
+	github.com/golang/snappy v0.0.0-20180518054509-2e65f85255db // indirect
+	github.com/pierrec/lz4 v2.0.5+incompatible // indirect
+	github.com/rcrowley/go-metrics v0.0.0-20180503174638-e2704e165165 // indirect
+)
